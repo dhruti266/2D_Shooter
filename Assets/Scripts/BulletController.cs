a72152fd34aa5ac2f522e2a755566ab90ca5912f
Dhruti Parekh <dhruti.parekh266@gmail.com>
2017-10-15 19:45:44 -0400

    Added bullet firing functionality

---
+++ Assets/Scripts/BulletController.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BulletController : MonoBehaviour {
+
+	public GameObject bullet;
+	public Transform _transform;
+
+	void FixedUpdate(){
+
+		bool fire = Input.GetButtonDown ("Fire1");// when mouse left button is pressed
+
+		if (fire)
+			Instantiate (bullet, _transform.position, _transform.rotation);
+	}
+}
---
+++ Assets/Scripts/FirePointController.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FirePointController : MonoBehaviour {
+
+	public float bulletForce = 10000f;
+
+	public void OnTriggerEnter2D(Collider2D target){
+
+		if (target.gameObject.tag == "plane")
+			GetComponent<Rigidbody2D> ().AddForce (transform.right * bulletForce);
+	}
+
+}
