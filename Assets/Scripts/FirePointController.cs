d4a47995dbcb7bac6c92f98dea390049c80986f1
Dhruti Parekh <dhruti.parekh266@gmail.com>
2017-10-15 20:19:25 -0400

    Some corrections to bullet firing functionality

--- Assets/Scripts/FirePointController.cs
+++ Assets/Scripts/FirePointController.cs
@@ -5,6 +5,22 @@ using UnityEngine;
 public class FirePointController : MonoBehaviour {
 
 	public float bulletForce = 10000f;
+	public Transform _bullet;
+//	public float collisionRadius = 0.5f;
+//	public bool collision = false;
+//	public LayerMask collideWith;
+
+	void FixedUpdate (){
+//		collision = Physics2D.OverlapCircle (_bullet.position, collisionRadius,	collideWith);
+
+//		//destroys the bullet when collision occurs with ground
+//		if (collision)
+//			Destroy (gameObject);
+
+		// destroy the bullet when it is not visible in camera view
+		if (!GetComponent<Renderer> ().isVisible)
+			Destroy (gameObject);
+	}
 
 	public void OnTriggerEnter2D(Collider2D target){
 
